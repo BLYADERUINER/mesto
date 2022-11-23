cc0313e8c3397bd56e059fcdd1b24849464225ce
Dmitry <Ezzy-Way@yandex.ru>
2022-11-23 23:41:17 +0400

    add/popupElementsCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,33 @@
-const popupElement = document.querySelector('.pop-up'); // переменная поп-пуп
-const popupCloseButtonElement = popupElement.querySelector('.pop-up__button-close'); // переменная кнопки закрытия поп-пуп
-const profileSection = document.querySelector('.profile'); // переменная секции профиля
-const popupOpenButtonElement = profileSection.querySelector('.profile__button-edit'); // переменная кнопки профиля редактировать
+// pop-up PROFILE-EDIT;
+const popupProfileEdit = document.querySelector('.pop-up__profile-edit'); // переменная поп-пуп редактирования профиля
+const popupCloseProfileEdit = popupProfileEdit.querySelector('.pop-up__button-close'); // переменная закрытия поп-пуп профиля
 
-const openPopup = function() {   // функция открытия ред.профиля
-  popupElement.classList.add('pop-up_opened');
-  nameInput.value = profileName.textContent;
-  statusInput.value = profileStatus.textContent;
+// pop-up ELEMENTS-CARD;
+const popupElementsCard = document.querySelector('.pop-up__elements'); // переменная поп-пуп карточек
+const popupCloseElementsCard = popupElementsCard.querySelector('.pop-up__button-close'); // переменная закрытия поп-пуп карточек
+
+// Profile section
+const profileSection = document.querySelector('.profile'); // переменная секции профиля
+const popupOpenProfileEdit = profileSection.querySelector('.profile__button-edit'); // переменная кнопки (open) редактирования профиля
+const popupOpenElementsCard = profileSection.querySelector('.profile__button-add'); // переменная кнопки (open) добавления карточек
+const profileName = profileSection.querySelector('.profile__name'); // переменная имя профиля
+const profileStatus = profileSection.querySelector('.profile__status'); // переменная статуса профиля
+
+// pop-up Form
+const popupFormElement = popupProfileEdit.querySelector('.pop-up__form'); // переменная поппуп формы
+const nameInput = popupFormElement.querySelector('.pop-up__input_name_edit'); // переменная ввода имени поп-пуп
+const statusInput = popupFormElement.querySelector('.pop-up__input_status_edit'); // переменная ввода статуса поп-пуп
+
+const openPopup = function (popup) {   // функция открытия ред.профиля
+  popup.classList.add('pop-up_opened');
+  if (popupProfileEdit) { // если открывается ред.проф
+   nameInput.value = profileName.textContent;
+   statusInput.value = profileStatus.textContent;
+  }
 }
 
-const closePopup = function() {   // функция закрытия ред.профиля
-  popupElement.classList.remove('pop-up_opened');
+const closePopup = function (popup) {   // функция закрытия ред.профиля
+  popup.classList.remove('pop-up_opened');
 }
 
 // const closePopupOnOverlay = function(event) {   // функция закрытия ред.профиля вне модального окна
@@ -20,11 +37,15 @@ const closePopup = function() {   // функция закрытия ред.пр
 // }
 
 
-const popupFormElement = popupElement.querySelector('.pop-up__form'); // переменная поппуп формы
-const profileName = profileSection.querySelector('.profile__name'); // переменная имя профиля
-const profileStatus = profileSection.querySelector('.profile__status'); // переменная статуса профиля
-const nameInput = popupFormElement.querySelector('.pop-up__input_name_edit'); // переменная ввода имени поп-пуп
-const statusInput = popupFormElement.querySelector('.pop-up__input_status_edit'); // переменная ввода статуса поп-пуп
+// const likeButtons = document.querySelectorAll('.element__button'); //ебаные лайки
+
+// for (let likeButton of likeButtons) {
+//   likeButton.addEventListener('click', function() {
+//     alert('Нахуй ты жмал ?');
+//     likeButton.classList.toggle('element__button_active');
+//   })
+// }
+
 
 
 
@@ -36,8 +57,22 @@ function popupFormSubmitHandler (evt) { // функция отправки
 }
 
 
+popupOpenProfileEdit.addEventListener('click', function () { // обработчик открытия попупы редактирования профиля
+  openPopup(popupProfileEdit);
+});
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-// popupElement.addEventListener('click', closePopupOnOverlay);
-popupFormElement.addEventListener('submit', popupFormSubmitHandler);
+popupCloseProfileEdit.addEventListener('click', function () { // обработчик закрытия попупы ред.проф.
+  closePopup(popupProfileEdit);
+});
+
+popupOpenElementsCard.addEventListener('click', function () { // обработчик открытия попупы добавления элементов карт
+  openPopup(popupElementsCard);
+})
+
+popupCloseElementsCard.addEventListener('click', function () { // обработчик закрытия попупы элементов карт
+  closePopup(popupElementsCard);
+});
+
+// popupProfileEdit.addEventListener('click', closePopupOnOverlay);
+
+popupFormElement.addEventListener('submit', popupFormSubmitHandler); // обработчик отправки формы ред.профиля
