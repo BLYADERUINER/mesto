a3869b60f92065d0e29a8d17dd1f6b5887489dfe
Dmitry <Ezzy-Way@yandex.ru>
2022-11-26 19:58:25 +0400

    fixed/images.2

--- scripts/index.js
+++ scripts/index.js
@@ -38,27 +38,27 @@ const elementCardTemplate = document.querySelector('#card').content; // шабл
 const cardsArray = [ // массив начальных карт
   {
     name: 'Карачевск',
-    link: './images/elements/image1.jpg'
+    link: 'https://images.unsplash.com/photo-1538819285938-6a9b4eda500b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80'
   },
   {
     name: 'Гора Эльбрус',
-    link: './images/elements/image2.jpg'
+    link: 'https://images.unsplash.com/photo-1638989432598-78740c9ba7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80'
   },
   {
     name: 'Домбай',
-    link: './images/elements/image3.jpg'
+    link: 'https://images.unsplash.com/photo-1617911478446-c7f1dd96966e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
   },
   {
     name: 'Камчатка',
-    link: './images/elements/image4.jpg'
+    link: 'https://images.unsplash.com/photo-1557771810-22b35659143c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
   },
   {
     name: 'Байкал',
-    link: './images/elements/image5.jpg'
+    link: 'https://images.unsplash.com/photo-1552735855-557bdba3961a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80'
   },
   {
     name: 'Антарктика',
-    link: './images/elements/image6.jpg'
+    link: 'https://images.unsplash.com/photo-1551415923-58aba9efeb2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
   }
 ];
 
@@ -82,9 +82,11 @@ const createCard = function (card) { // функция создания карт
 
   cardTitle.textContent = card.name; // обозначаем равенство значению имени массива
   cardImage.src = card.link; // и ссылку на картинку
+  cardImage.alt = card.name; // и альтернативный текст из названия
 
   cardImage.addEventListener('click', function () { // обработчик клика по картинке
     popupImage.src = cardImage.src; // пусть поппуп картинка равна картинке карточке
+    popupImage.alt = cardTitle.textContent; // альтернативный текст равен тексту карточке
     popupFigcaption.textContent = cardTitle.textContent; // подпись равна тексту карточке
     openPopup(popupCardImage); // открываем попап картинки
   });
