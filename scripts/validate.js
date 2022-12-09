fcae8dcbc4c248834b8a98f8ded3cdc31fb22f53
Dmitry <Ezzy-Way@yandex.ru>
2022-12-09 19:02:02 +0400

    add/errorStyle

---
+++ scripts/validate.js
@@ -0,0 +1,14 @@
+// const objValidate = {
+//     formSelector: '.pop-up__form',
+//     inputSelector: '.pop-up__input',
+//     submitButtonSelector: '.pop-up__button-save',
+//     inactiveButtonClass: 'pop-up__button-save_disabled',
+//     inputErrorClass: 'pop-up__input_type_error',
+//     errorClass: 'pop-up__error_visible'
+// };
+
+// enableValidation(objValidate);
+
+// function enableValidation({}) {
+
+// };
